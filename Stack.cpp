ed5d6d7cf1bd699f499b65c08dfdefbbde3d0fcd
Michel Fang <mfang555@gmail.com>
2022-02-02 19:55:17 +0700

    impl constructors

--- Stack.cpp
+++ Stack.cpp
@@ -2,13 +2,26 @@
 #include <iostream>
 
 Stack::Stack() {
+    this->capacity = 10;
+    this->size = 0;
+    this->data = new int[this->capacity];
 }
 
 Stack::Stack(int cap) {
+    this->capacity = cap;
+    this->size = 0;
+    this->data = new int[this->capacity];
 }
 
-Stack::Stack(const Stack& b) {
+Stack::Stack(const Stack& s) {
+    this->capacity = s.capacity;
+    this->size = s.size;
+    this->data = new int[s.capacity];
+    for (int i = 0; i < this->capacity; i++) {
+        this->data[i] = s.data[i];
+    }
 }
+
 
 Stack::~Stack() {
 }
