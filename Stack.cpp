178f6f5b45fd5c637c19c98b93aee5b5d9cc7d1b
Michel Fang <mfang555@gmail.com>
2022-02-02 19:48:56 +0700

    rename and add Stack dummy impl

---
+++ Stack.cpp
@@ -0,0 +1,21 @@
+#include "Stack.hpp"
+#include <iostream>
+
+Stack::Stack() {
+}
+
+Stack::Stack(int cap) {
+}
+
+Stack::Stack(const Stack& b) {
+}
+
+Stack::~Stack() {
+}
+
+void Stack::push(int x) {
+}
+
+int Stack::pop() {
+    return 2210;
+}
--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
-#include "stack.h"
+#include "Stack.hpp"
 using namespace std;
 
 int main() {
   cout << "punten bang" << endl;
+
+  Stack s;
+  cout << s.pop() << '\n';
   return 0;
 }
